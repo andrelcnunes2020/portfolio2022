7cb835674d058ff37a0dfad0c1df47f6de68c3eb
André Luiz <andre.doctors@gmail.com>
2022-05-15 13:15:51 -0300

    adicionando scrollreveal

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('#home', { duration: 2000 })
+
+sr.reveal('#skills', {
+  rotate: { x: 80, y: 0, z: 0 },
+  duration: 3000
+})
+
+sr.reveal('#projetos', { duration: 3000 })
+
+sr.reveal('#contato', { duration: 3000 })
