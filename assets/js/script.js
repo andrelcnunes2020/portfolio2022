a0fcd31782615af36e49c2ecaa8ae9ef57d3417a
André Luiz <andre.doctors@gmail.com>
2022-05-15 18:42:24 -0300

    ajustando o footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,9 @@ window.sr = ScrollReveal({ reset: true })
 
 sr.reveal('#home', { duration: 2000 })
 
-sr.reveal('#skills', {
-  rotate: { x: 80, y: 0, z: 0 },
-  duration: 3000
-})
+sr.reveal('#skills', { duration: 3000 })
+
+sr.reveal('#tecnology', { duration: 4000 })
 
 sr.reveal('#projetos', { duration: 3000 })
 
